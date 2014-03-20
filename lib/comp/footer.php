6c2c3c5918118f1bc279677308b52bf28d1b2d2c
Julio Ghisloti <julioghisloti@gmail.com>
2014-03-20 09:37:01 -0300

    lib

---
+++ lib/comp/footer.php
@@ -0,0 +1,25 @@
+<!-- start -footer- -->
+	<footer class="footer">
+
+		<!-- start -bts- -->
+			<div class="footer_content">
+				
+				<!-- Loja -->
+				<a href="">LOJAS</a>
+
+				<!-- Fale conosco -->
+				<a href="">FALE CONOSCO</a>
+
+				<!-- Busca -->
+				<form action="#" method="post">
+					<input type="text" name="buscar" placeholder="BUSCA" />
+					<button>
+						<img src="lib/img/buscar.png" alt="Botão de buscar">
+					</button>
+				</form>
+
+			</div>
+		<!-- end -bts- -->
+
+	</footer>
+<!-- end -footer- -->
---
+++ lib/comp/top.php
@@ -0,0 +1,11 @@
+<!-- start -top- -->
+	<section class="top">
+		
+		<!-- vitrine logo -->
+		<div class="vitrine_logo"><img src="lib/img/vitrine_logo.png" alt="Vitrine Unimart"></div>
+
+		<!-- unimart logo -->
+		<div class="unimart_logo"><img src="lib/img/logo.png" alt="Unimart"></div>
+
+	</section>
+<!-- start -top- -->
